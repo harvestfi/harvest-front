6cbf3d99cd23077b93d070b3c84bd38e9acf4dd6
hubert895 <95780576+hubert895@users.noreply.github.com>
2022-04-07 18:22:28 +0200

    Deployment adjustments (#16)

--- check-updates.sh
+++ check-updates.sh
@@ -1,3 +1,7 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 while true; do
     if git pull | grep -q 'Already up to date.'; then
         echo "Not git updates to pull"
