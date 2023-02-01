f597f1365fb99da33d174dbd1eebff72caa0d1e9
CryptJS13 <jasper@slimmens.nl>
2023-02-01 16:23:09 +0100

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const helmet = require('helmet')
 const path = require('path')
 
-const builtDirectory = path.join(__dirname, 'prod')
+const builtDirectory = path.join(__dirname, 'build')
 const PORT = process.env.PORT || '5000'
 const app = express()
 
