25aed7d83f455852ab149ab6444c3008ba86f8e7
CryptJS13 <jasper@slimmens.nl>
2023-02-01 16:20:39 +0100

    undo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const helmet = require('helmet')
 const path = require('path')
 
-const builtDirectory = path.join(__dirname, 'build')
+const builtDirectory = path.join(__dirname, 'prod')
 const PORT = process.env.PORT || '5000'
 const app = express()
 
