c3a9fd41a6b6247514c4a1069cbe9984239703b0
CryptJS13 <jasper@slimmens.nl>
2023-02-02 09:56:41 +0100

    change app setting

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const builtDirectory = path.join(__dirname, 'build')
 const PORT = process.env.PORT || '5000'
 const app = express()
 
+app.disable('x-powered-by')
 app.use(
   helmet({
     crossOriginEmbedderPolicy: false,
