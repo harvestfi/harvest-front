ecfeeda32113d3ba110d704b1d57d2ae9c7f9394
William Liu <william.liu@klinkfinance.com>
2023-03-30 16:21:50 +0000

    remove test url

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(
     // contentSecurityPolicy: false,
     contentSecurityPolicy: {
       directives: {
-        frameAncestors: ['https://dapp-browser.apps.ledger.com/', 'http://localhost:3000'],
+        frameAncestors: ['https://dapp-browser.apps.ledger.com/'],
         scriptSrc: ["'self'", "'unsafe-inline'", 'https://static.cloudflareinsights.com'],
         connectSrc: ["'self'", '*'],
       },
